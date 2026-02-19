c36d52bc08bf6177030e6d287f6b79090bda7a09
vishnu20060930 <vishnuvardhanmudhiraj67@gmail.com>
2026-02-19 21:48:38 +0530

    Add files via upload

---
+++ basic calculations.py
@@ -0,0 +1,11 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+
+if num2 != 0:
+    print("Division:", num1 / num2)
+else:
+    print("Division by zero not allowed")
